2aa8f7a4e5506477147652f74ecdd29bc6435b27
glinren <glinren.kre@gmail.com>
2021-02-27 16:51:52 +0100

    files from Flux++ project

---
+++ mem_comparable_closure/premake5.lua
@@ -0,0 +1,22 @@
+GOOGLE_BENCHMARK_LIBDIRS = {}
+
+workspace "mem_comparable_closure"
+    configurations { "Test"}
+    targetdir "bin"
+
+
+project "mem_comparable_closure"
+    kind "ConsoleApp"
+    toolset "clang"
+    language "C++"
+    cppdialect "C++17"
+    files { "src/*.cpp"  }
+    includedirs {"include", "src/include"}
+    removefiles { "libs/allocators/*.t.cpp"}
+    buildoptions { "-Wall", "-Wno-unused-local-typedef"}
+    optimize "Full"
+    filter "Test"
+        filename "Test"
+        files {"test/*.cpp"}
+	includedirs "libs/doctest/doctest"
+	targetdir "bin/Test"
